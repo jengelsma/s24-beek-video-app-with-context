5a542c8011e6b15f1ac7c3393e38c6bca5dd4765
Jonathan Engelsma <jengelsma@gmail.com>
2024-05-28 09:06:11 -0400

    Completed Part F

--- screens/VideoListScreen.js
+++ screens/VideoListScreen.js
@@ -1,28 +1,43 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { BaseRouter } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
 import { getVideos } from '../api/YTServer';
 
 const VideoListScreen = ({ navigation }) => {
+  const [videos, setVideos] = useState([]);
+
   useEffect(() => {
     getVideos((data) => {
-      console.log('received: ', data);
+      setVideos(data.items);
     });
   }, []);
 
+  const renderVideo = ({ index, item }) => {
+    return (
+      <View>
+        <Text> {item.snippet.title} </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Button
-        title='Goto YTViewerScreen'
-        onPress={() => {
-          navigation.navigate('Video Viewer', 'video to be displayed');
-        }}
+    <View style={styles.screen}>
+      <FlatList
+        data={videos}
+        keyExtractor={(item) => item.id.videoId}
+        extraData={videos}
+        renderItem={renderVideo}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    width: '100%',
+  },
+});
 
 export default VideoListScreen;
