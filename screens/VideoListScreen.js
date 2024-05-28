25935a7757e262375dc7dfd6d05118c9782ae8a4
Jonathan Engelsma <jengelsma@gmail.com>
2024-05-28 09:19:37 -0400

    Completed Part G

--- screens/VideoListScreen.js
+++ screens/VideoListScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import { BaseRouter } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
 import { getVideos } from '../api/YTServer';
+import { Image, ListItem } from '@rneui/themed';
 
 const VideoListScreen = ({ navigation }) => {
   const [videos, setVideos] = useState([]);
@@ -15,9 +22,22 @@ const VideoListScreen = ({ navigation }) => {
 
   const renderVideo = ({ index, item }) => {
     return (
-      <View>
-        <Text> {item.snippet.title} </Text>
-      </View>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Video Viewer', item);
+        }}
+      >
+        <ListItem key={index}>
+          <Image
+            source={{ uri: item.snippet.thumbnails.default.url }}
+            style={{ width: 100, height: 55 }}
+          />
+          <ListItem.Content>
+            <ListItem.Title> {item.snippet.title} </ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+      </TouchableOpacity>
     );
   };
 
