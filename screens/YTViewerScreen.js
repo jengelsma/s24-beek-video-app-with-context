7d9459ed776331c06fd5cad3015f1760b651e807
Jonathan Engelsma <jengelsma@gmail.com>
2024-06-03 10:31:51 -0400

    Completed Part 10E - accessing and updating state

--- screens/YTViewerScreen.js
+++ screens/YTViewerScreen.js
@@ -1,16 +1,50 @@
+import { StyleSheet, View } from 'react-native';
+import { useContext, useLayoutEffect } from 'react';
+
+import HeaderButton from '../components/HeaderButton';
+import { PlayLaterContext } from '../context/PlayLaterContext';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const YTViewerScreen = ({ route }) => {
-  console.log('YTViewerScreen: ', route.params);
+const YTViewerScreen = ({ navigation, route }) => {
+  const playLaterContext = useContext(PlayLaterContext);
+  const video = route.params;
+  const videoId = video.id.videoId;
+  let videoIsSaved = false;
+
+  if (playLaterContext.videos.some((v) => v.id.videoId === videoId)) {
+    videoIsSaved = true;
+  }
+
+  function changePlayLaterStatusHandler() {
+    if (videoIsSaved) {
+      playLaterContext.deleteVideo(videoId);
+    } else {
+      playLaterContext.addVideo(video);
+    }
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <HeaderButton
+            icon={videoIsSaved ? 'bookmark' : 'bookmark-outline'}
+            color='blue'
+            onPress={changePlayLaterStatusHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, changePlayLaterStatusHandler]);
+
   return (
     <View style={styles.screen}>
       <WebView
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
+        javaScriptEnabled
+        domStorageEnabled
         source={{
-          uri: `https://www.youtube.com/embed/${route.params.id.videoId}`,
+          uri: `https://www.youtube.com/embed/${videoId}`,
         }}
       />
     </View>
