c2cdeb8a005e1cf3dcb1f2026f95004854c0a5b9
Jonathan Engelsma <jengelsma@gmail.com>
2024-06-03 10:15:51 -0400

    Lecture 10C starts here!

--- App.js
+++ App.js
@@ -1,22 +1,66 @@
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import VideoListScreen from './screens/VideoListScreen';
+import WatchLaterScreen from './screens/WatchLaterScreen';
 import YTViewerScreen from './screens/YTViewerScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Browse Videos'
+        component={VideoListScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name='Video Viewer'
+        component={YTViewerScreen}
+        options={{
+          title: '',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Video List'
-          component={VideoListScreen}
-          options={{ title: 'Beekeeping Videos' }}
+      <Drawer.Navigator
+        options={{
+          headerShown: true,
+        }}
+      >
+        <Drawer.Screen
+          name='VideoBrowser'
+          component={StackNavigator}
+          options={{
+            title: 'Video Browser',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='videocam' color={color} size={size} />
+            ),
+          }}
         />
-        <Stack.Screen name='Video Viewer' component={YTViewerScreen} />
-      </Stack.Navigator>
+        <Drawer.Screen
+          name='WatchLater'
+          component={WatchLaterScreen}
+          options={{
+            title: 'Watch Later Videos',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='time' color={color} size={size} />
+            ),
+          }}
+        />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
